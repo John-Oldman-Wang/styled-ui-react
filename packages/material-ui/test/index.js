366c7715d9e4bb15933cdddc7d4c026d9b1cf714
王琳 <mag1c_shen_09@foxmail.com>
2019-03-04 00:11:58 +0800

    添加测试材料

--- packages/material-ui/test/index.js
+++ packages/material-ui/test/index.js
@@ -4,9 +4,11 @@ const root = document.getElementById('root');
 
 import MPaper from '@material-ui/core/Paper';
 import MAppBar from '@material-ui/core/AppBar';
+import MAvatar from '@material-ui/core/Avatar';
 
 import Paper from '../src/Paper/index';
 import AppBar from '../src/AppBar/index';
+import Avatar from '../src/Avatar/index';
 
 class Test extends React.Component {
   constructor() {
@@ -22,6 +24,7 @@ class Test extends React.Component {
         <MPaper ref={this.m} square elevation={2}>
           asdasd
         </MPaper>
+        <MAvatar alt="Remy Sharp" src="/static/1.jpg" />
         <div>-------------------------</div>
         <div>-------------------------</div>
         <div>-------------------------</div>
@@ -29,6 +32,7 @@ class Test extends React.Component {
         <Paper ref={this.t} square elevation={2}>
           asdasd
         </Paper>
+        <Avatar alt="Remy Sharp" src="/static/1.jpg" />
       </>
     );
   }
