299a46eab20e74e86eafb8cf7babedefe7d2d8e6
王琳 <mag1c_shen_09@foxmail.com>
2019-03-12 16:30:24 +0800

    Revert "添加打包配置"

--- packages/material-ui/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as AppBar } from './AppBar/index';
-export { default as Avatar } from './Avatar/index';
-export { default as Backdrop } from './Backdrop/index';
-export { default as Badge } from './Badge/index';
-export { default as BottomNavigation } from './BottomNavigation/index';
-export { default as Fade } from './Fade/index';
-export { default as Paper } from './Paper/index';
