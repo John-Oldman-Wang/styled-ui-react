2657bdcabd33718c10dad647745ba21e68274ccb
wanglin.john <wanglin.john@bytedance.com>
2019-03-23 21:58:51 +0800

    test code

--- packages/material-ui/test/index.js
+++ packages/material-ui/test/index.js
@@ -1,71 +1,58 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import MPaper from '@material-ui/core/Paper';
-import MAppBar from '@material-ui/core/AppBar';
-import MAvatar from '@material-ui/core/Avatar';
-import MFade from '@material-ui/core/Fade';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import Icon from '@material-ui/core/Icon';
 
-import Paper from '../src/Paper/index';
-import AppBar from '../src/AppBar/index';
-import Avatar from '../src/Avatar/index';
-import Fade from '../src/Fade/index';
-import Badge from '../src/Badge/index';
+import MyBottomNavigation from '../lib/BottomNavigation';
+import MyBottomNavigationAction from '../lib/BottomNavigationAction';
 
+import * as A from '@material-ui/core';
+import * as B from '../lib/index';
+
+const bb = Object.keys(B);
+console.log(
+  Object.keys(A)
+    .map((i) => {
+      if (bb.includes(i)) {
+        return `- [x] ${i}`;
+      }
+      return `- [ ] ${i}`;
+    })
+    .join('\n')
+);
 const root = document.getElementById('root');
 class Test extends React.Component {
   constructor() {
     super();
     this.state = {
-      flag: false
+      value: 'recents'
     };
-    window.t = this;
-    this.m = React.createRef();
-    this.t = React.createRef();
+  }
+  handleChange(key) {
+    return (event, value) => {
+      this.setState({
+        [key]: value
+      });
+    };
   }
   render() {
+    const { value } = this.state;
     return (
       <>
-        {/* <MAppBar position="fixed">MAppBar</MAppBar> */}
-        <MPaper ref={this.m} square elevation={2}>
-          asdasd
-        </MPaper>
-        <MFade onEnter={() => {}} b={'b'} className="t" in={this.state.flag} a={'a'}>
-          <div>
-            <div>-------------------------</div>
-            <div>-------------------------</div>
-            <div>-------------------------</div>
-          </div>
-        </MFade>
-        <MAvatar alt="Remy Sharp" src="/static/1.jpg" />
-        <div>-------------------------</div>
-        <div>-------------------------</div>
-        <div>-------------------------</div>
-        {/* <AppBar position="fixed">MAppBar</AppBar> */}
-        <Paper ref={this.t} square elevation={2}>
-          asdasd
-        </Paper>
-        <Badge badgeContent={4} color="primary">
-          a
-        </Badge>
-        <Avatar alt="Remy Sharp" src="/static/1.jpg" />
-        <Fade
-          onEnter={() => {}}
-          b={'b'}
-          className="t"
-          in={this.state.flag}
-          a={'a'}
-          onClick={() => {
-            this.setState({
-              flag: !this.state.flag
-            });
-          }}>
-          <div>
-            <div>-------------------------</div>
-            <div>-------------------------</div>
-            <div>-------------------------</div>
-          </div>
-        </Fade>
+        <BottomNavigation value={value} onChange={this.handleChange('value')}>
+          <BottomNavigationAction label="Recents" value="recents" showLabel icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+        </BottomNavigation>
+
+        <MyBottomNavigation value={value} onChange={this.handleChange('value')}>
+          <MyBottomNavigationAction label="Recents" value="recents" showLabel icon={<RestoreIcon />} />
+          <MyBottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+        </MyBottomNavigation>
       </>
     );
   }
