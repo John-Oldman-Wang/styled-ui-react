e9e3d4e7a7a4d63288e1ca96e0e08556038aba4c
wanglin.john <wanglin.john@bytedance.com>
2019-03-29 12:32:34 +0800

    添加 Button -> index

--- packages/material-ui/src/index.ts
+++ packages/material-ui/src/index.ts
@@ -4,6 +4,7 @@ export { default as Backdrop } from './Backdrop/index';
 export { default as Badge } from './Badge/index';
 export { default as BottomNavigation } from './BottomNavigation/index';
 export { default as BottomNavigationAction } from './BottomNavigationAction/index';
+export { default as Button } from './Button/index';
 export { default as ButtonBase } from './ButtonBase/index';
 export { default as Fade } from './Fade/index';
 export { default as NoSsr } from './NoSsr/index';
